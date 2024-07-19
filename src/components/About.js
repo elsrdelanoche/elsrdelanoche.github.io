0e30534a6956c02e89e0d99c3135d04004c2c2d5
Alfredo Bautista <alfredobr587@gmail.com>
2024-07-18 19:58:12 -0600

    logo, correcciones a about

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,9 @@ import Batman from '../../src/IMG/Alfredo.jpg'; // Asegúrate de importar la ima
 
 function About() {
   return (
-    <div className="container about">
+    <div className="container about py-5">
       <div className="row align-items-center">
-        <div className="col-md-5 col-12 text-center text-md-start">
+        <div className="col-md-7 col-12 text-center text-md-start">
           <div className="about-me-text"> {/* Contenedor para el texto */}
             <div className="fs-3 fw-light text-muted">Hello there...</div>
             <h1 className="display-3 fw-bolder mb-5">
@@ -15,14 +15,14 @@ function About() {
             <p>
               I am a computer science student who enjoys understanding and exploring the potential of the technologies at my disposal. With great fascination, I have observed and learned from the advancements in this field, and I am eager to contribute by designing my own systems to address new challenges.
             </p>
-            <div class="d-flex fs-2 gap-4">
-                <a class="text-gradient" href="#!"><i class="bi bi-twitter"></i></a>
-                <a class="text-gradient" href="#!"><i class="bi bi-linkedin"></i></a>
-                <a class="text-gradient" href="#!"><i class="bi bi-github"></i></a>
+            <div class="btn-group fs-2 gap-4">
+                <a class="btn-link text-gradient" href="#!"><i class="bi bi-twitter"></i></a>
+                <a class="btn-link text-gradient" href="#!"><i class="bi bi-linkedin"></i></a>
+                <a class="btn-link text-gradient" href="#!"><i class="bi bi-github"></i></a>
             </div>
           </div>
         </div>
-        <div className="col-md-7 col-12">
+        <div className="col-md-5 col-12">
           <div className="about-me-image d-flex justify-content-center mt-5 mt-md-0"> {/* Contenedor para la imagen */}
             <div className="profile bg-gradient-primary-to-secondary">
               <img className="profile-img" src={Batman} alt="Profile" />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from '../../src/IMG/logo.png'; // Asegúrate de tener tu logo en la carpeta 'src'
+import logo from '../../src/IMG/transparentLogo.png'; // Asegúrate de tener tu logo en la carpeta 'src'
 import './Header.css';
 
 function Header() {
   return (
-    <header className="header">
+    <header className="header px-5">
       <div className="header-left">
-        <h1 className="header-name">elSrDeLaNoche</h1>
+        <h1 className="header-name">elsrdelanoche</h1>
       </div>
       <div className="header-right">
         <img src={logo} alt="Logo" className="header-logo" />
